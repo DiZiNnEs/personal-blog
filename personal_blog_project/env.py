86f8f14bba33e18897a467dd10f9dc185298c6a8
Iglek Kuat <dizinnes228@gmail.com>
2020-11-29 16:55:00 +0600

    feat: create env.py

---
+++ personal_blog_project/env.py
@@ -0,0 +1,11 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def env(key: str) -> str:
+    value = getenv(key)
+    if value is None:
+        raise EnvironmentError(f'The key with name {key} was not found in the env')
+    return value
