77608b4ae71c4d8347bae2d858a8212d69cf4be6
Iglek Kuat <dizinnes228@gmail.com>
2020-11-30 16:16:51 +0600

    fix: change value name

--- personal_blog_app/blog_api/api_urls.py
+++ personal_blog_app/blog_api/api_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .api_views import PostViewSet
 
 urlpatterns = [
-    path('posts/', PostViewSet.as_view({'get': 'get_posts'}), name='get_all_posts'),
+    path('posts/', PostViewSet.as_view({'get': 'get_all_posts'}), name='get_all_posts'),
     path('post/<int:pk>/', PostViewSet.as_view({'get': 'get_certain_post'}), name='get_certain_post'),
 ]
