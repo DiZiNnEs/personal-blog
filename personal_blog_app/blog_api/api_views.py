892d14d770ed9dc4b527cf6542d4fd3f6f92b3c1
Iglek Kuat <dizinnes228@gmail.com>
2020-11-30 15:02:22 +0600

    feat: create new function

--- personal_blog_app/blog_api/api_views.py
+++ personal_blog_app/blog_api/api_views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .api_serializers import PostSerializer
 
+from .api_serializers import PostSerializer
 from ..models import Post
 
 
 class PostViewSet(viewsets.ViewSet):
-    def get_posts(self, request):
+    def get_all_posts(self, request):
         queryset = Post.objects.all()
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def get_certain_post(self, request, pk):
+        queryset = Post.objects.get(slug=pk)
+        serializer = PostSerializer(queryset)
+        return Response(serializer.data)
+
