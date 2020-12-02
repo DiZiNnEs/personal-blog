e8bf7236b6db8fb93cb2b202374f138bf38317b2
Iglek Kuat <dizinnes228@gmail.com>
2020-12-02 13:00:00 +0600

    feat: add post view

--- personal_blog_app/urls.py
+++ personal_blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from personal_blog_app.views import HomeView
+from personal_blog_app.views import HomeView, PostView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='index')
+    path('', HomeView.as_view(), name='index'),
+    path('post/<int:pk>', PostView.as_view(), name='post_view'),
 ]
--- personal_blog_app/views.py
+++ personal_blog_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Post
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class PostView(TemplateView):
+    template_name = 'post.html'
