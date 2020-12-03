08f17bb9db00ddcbc53df8094b52587b6faf00e0
Iglek Kuat <dizinnes228@gmail.com>
2020-12-04 00:47:19 +0600

    feat: add try except

--- personal_blog_app/blog_api/api_views.py
+++ personal_blog_app/blog_api/api_views.py
@@ -1,13 +1,18 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-
 from .api_serializers import PostsSerializer, PostSerializer
 from ..models import Post
 
 
 class PostsViewSet(viewsets.ViewSet):
     def get_posts(self, request):
+        try:
+            return self.__get_posts()
+        except Exception as ex:
+            raise ex
+
+    def __get_posts(self):
         queryset = Post.objects.all().order_by('-publication_date')
         serializer = PostsSerializer(queryset, many=True)
         return Response(serializer.data)
@@ -15,6 +20,12 @@ class PostsViewSet(viewsets.ViewSet):
 
 class PostViewSet(viewsets.ViewSet):
     def get_post(self, request, pk):
+        try:
+            return self.__get_post(pk)
+        except Exception as ex:
+            raise ex
+
+    def __get_post(self, pk):
         queryset = Post.objects.get(slug=pk)
         serializer = PostSerializer(queryset)
         return Response(serializer.data)
